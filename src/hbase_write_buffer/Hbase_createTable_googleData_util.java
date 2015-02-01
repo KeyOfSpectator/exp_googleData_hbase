9a794283f504179e45c95c7f0fde1b9b80a7f453
KeyOfSpectator <413070456@qq.com>
2015-02-01 19:58:49 +0800

    add thoughput_hbase_32threads_50Wline.jar

---
+++ src/hbase_write_buffer/Hbase_createTable_googleData_util.java
@@ -0,0 +1,179 @@
+package hbase_write_buffer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class Hbase_createTable_googleData_util {
+
+	/*
+	 * this is the util class create the hbase table 
+	 * args[0] is the hdfsSite_PathStr (String) 
+	 * args[1] is the Configuration (Configuration)
+	 * args[2] is the HTableDescriptor (HTableDescriptor) 
+	 * args[3] is the tableName (String) 
+	 * args[4] is the startRowKey (int) 
+	 * args[5] is the endRowKey (int) 
+	 * args[6] is the splitNum (int)
+	 */
+
+	public static void hbase_createTable(String hdfsSite_PathStr,
+			Configuration conf, HTableDescriptor htd, String tableName,
+			int StartRowKey, int EndRowKey, int splitNum) throws IOException {
+
+//		String StartRowKey_str = String.valueOf(StartRowKey);
+		String EndRowKey_str = String.valueOf(EndRowKey);
+		int RowKeyLength = EndRowKey_str.length();
+		String StartRowKey_str = intToString(StartRowKey);
+
+		// Configuration conf = HBaseConfiguration.create();
+		conf.addResource(new Path(hdfsSite_PathStr));
+		// conf.addResource("/usr/local/hbase/conf/hdfs-site.xml");
+		HBaseAdmin admin = new HBaseAdmin(conf);
+		// String tableName = "tableName";
+		String familyName = "colFamily";
+		String col0 = "time";
+		String col1 = "missing";
+		String col2 = "job";
+		String col3 = "task";
+		String col4 = "machine";
+		String col5 = "event";
+		String col6 = "user";
+		String col7 = "scheduling";
+		String col8 = "priority";
+		String col9 = "cpu";
+		String col10 = "memory";
+		String col11 = "disk";
+		String col12 = "different";
+
+		// HTableDescriptor htd = new HTableDescriptor(tableName);
+		HColumnDescriptor hdc = new HColumnDescriptor(familyName);
+		htd.addFamily(hdc);
+		long before = System.currentTimeMillis();
+		// admin.createTable(htd,splits);
+
+		/*
+		 * public void createTable(HTableDescriptor desc, byte[] startKey,
+		 * byte[] endKey, int numRegions) throws IOException
+		 */
+
+		// i is the row key , split to 15 , row key range: 000000~499999
+		admin.createTable(htd, Bytes.toBytes(StartRowKey_str),
+				Bytes.toBytes(EndRowKey_str), splitNum);
+
+		long after = System.currentTimeMillis();
+		System.out.println("[Time] create table time :  " + (after - before));
+
+		/*
+		 * HTable table = new HTable(conf, htd.getName());
+		 * table.setAutoFlush(false); // table.setWriteBufferSize(209715200);
+		 * System.out.println("[Info] WriteBufferSize :  " +
+		 * table.getWriteBufferSize());
+		 * 
+		 * long begin = System.currentTimeMillis();
+		 * 
+		 * for (int i = 0; i < 50000; i++) { // byte[] kkk = new byte[10000 + i
+		 * / 1000]; // byte [] kkk=new byte[12];
+		 * 
+		 * /* Put(byte[] row) Create a Put operation for the specified row.
+		 */
+
+		/*
+		 * Put p1 = new Put(Bytes.toBytes(intToString(i)));
+		 * p1.setWriteToWAL(false);
+		 * 
+		 * /* add(byte[] family, byte[] qualifier, byte[] value) Add the
+		 * specified column and value to this Put operation.
+		 */
+
+		/*
+		 * 
+		 * p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col0),
+		 * data_list.get(i)[0]); p1.add(Bytes.toBytes(familyName),
+		 * Bytes.toBytes(col1), data_list.get(i)[1]);
+		 * p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col2),
+		 * data_list.get(i)[2]); p1.add(Bytes.toBytes(familyName),
+		 * Bytes.toBytes(col3), data_list.get(i)[3]);
+		 * p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col4),
+		 * data_list.get(i)[4]); p1.add(Bytes.toBytes(familyName),
+		 * Bytes.toBytes(col5), data_list.get(i)[5]);
+		 * p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col6),
+		 * data_list.get(i)[6]); p1.add(Bytes.toBytes(familyName),
+		 * Bytes.toBytes(col7), data_list.get(i)[7]);
+		 * p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col8),
+		 * data_list.get(i)[8]); p1.add(Bytes.toBytes(familyName),
+		 * Bytes.toBytes(col9), data_list.get(i)[9]);
+		 * p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col10),
+		 * data_list.get(i)[10]); p1.add(Bytes.toBytes(familyName),
+		 * Bytes.toBytes(col11), data_list.get(i)[11]);
+		 * p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col12),
+		 * data_list.get(i)[12]);
+		 * 
+		 * table.put(p1);
+		 * 
+		 * // System.out.println(i);
+		 * 
+		 * } long end = System.currentTimeMillis(); table.flushCommits();
+		 * System.out.println("[Time] HBase write time : " + (end - begin));
+		 */
+	}
+
+	static public String intToString(int x) {
+		String result = String.valueOf(x);
+		int size = result.length();
+		while (size < 6) {
+			size++;
+			result = "0" + result;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("[Test util] hbase_createTable ");
+
+		/*
+		 * this is the util class create the hbase table args[0] is the
+		 * hdfsSite_PathStr (String) args[1] is the Configuration
+		 * (Configuration) args[2] is the HTableDescriptor (HTableDescriptor)
+		 * args[3] is the tableName (String) args[4] is the startRowKey (int)
+		 * args[5] is the endRowKey (int) args[6] is the splitNum (int)
+		 */
+
+		System.out.print("/*\n" + " * this is the util class\n"
+				+ " * create the hbase table\n"
+				+ " * args[0] is the hdfsSite_PathStr 	(String)\n"
+				+ " * args    is the Configuration 		(Configuration)\n"
+				+ " * args    is the HTableDescriptor 		(HTableDescriptor)\n"
+				+ " * args[1] is the startRowKey		(int)\n"
+				+ " * args[2] is the startRowKey		(int)\n"
+				+ " * args[3] is the endRowKey		(int)\n"
+				+ " * args[4] is the splitNum 		(int)\n" + " * \n"
+				+ " * args[5] tablename for test    \n" + " */\n");
+
+		Configuration conf = HBaseConfiguration.create();
+		HTableDescriptor htd = new HTableDescriptor(args[5]);
+
+		try {
+			hbase_createTable(args[0], conf, htd, args[1],
+					Integer.parseInt(args[2]), Integer.parseInt(args[3]),
+					Integer.parseInt(args[4]));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/thread/OptThread.java
@@ -0,0 +1,167 @@
+package thread;
+
+import hbase_thoughput.Exp_thoughput_57M_32Threads;
+
+import java.nio.charset.Charset;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import com.csvreader.CsvReader;
+
+/*
+ * used by Exp_thoughput_57M_32Threads
+ * opt read and store the data
+ */
+
+public class OptThread extends Thread{
+	private int threadId;
+	private String csvFilePath;
+	private String site_PathStr;
+	private String tableName;
+	
+	static String familyName = "colFamily";
+	static String col0 = "time";
+	static String col1 = "missing";
+	static String col2 = "job";
+	static String col3 = "task";
+	static String col4 = "machine";
+	static String col5 = "event";
+	static String col6 = "user";
+	static String col7 = "scheduling";
+	static String col8 = "priority";
+	static String col9 = "cpu";
+	static String col10 = "memory";
+	static String col11 = "disk";
+	static String col12 = "different";
+	
+	public OptThread(int threadId , String FolderPath , String site_PathStr , String tableName){
+		this.threadId = threadId;
+		this.csvFilePath = FolderPath+String.valueOf(threadId)+".csv";
+		this.site_PathStr = site_PathStr;
+		this.tableName = tableName;
+	}
+	public void run(){
+		
+		try{
+			System.out.println("[Thread Info]"
+					+ " threadId = " + this.threadId
+					+ " started");
+			
+			Configuration conf = HBaseConfiguration.create();
+			
+			HTableDescriptor htd = new HTableDescriptor(tableName);
+			
+//			hbase_create_table( site_PathStr , tableName , conf , htd);
+			
+			try {
+				// String csvFilePath = "/home/fsc/ubuntu_exp/0.csv";
+				CsvReader reader = new CsvReader(csvFilePath, ',' , Charset.forName("SJIS")); // 一般用这编码读就可以了
+				
+				reader.readHeaders(); // 跳过表头 如果需要表头的话，不要写这句。
+				
+//				rowKey 
+				int i = 15625*threadId;
+				int i_origin = i;
+				
+				long total_write_time = 0;
+				
+				HTable table;
+				synchronized(Exp_thoughput_57M_32Threads.tableLock){
+					table = new HTable(conf, htd.getName());
+					table.setAutoFlush(false);
+					table.setWriteBufferSize(1024*1024*2);
+				}
+				// table.setWriteBufferSize(209715200);
+//				System.out.println("[Thread Info]"
+//						+ " threadId = " + this.threadId
+//						+ " WriteBufferSize :  " + table.getWriteBufferSize());
+				
+				System.out.println("[Thread Info]"
+						+ " threadId = " + this.threadId
+						+ " csvFilePath :  " + csvFilePath);
+				
+				
+				while (reader.readRecord()) { // 逐行读入除表头的数据
+					
+					long begin = System.currentTimeMillis();
+					
+						/*
+						 * Put(byte[] row)
+						 * Create a Put operation for the specified row.
+						 */
+						Put p1 = new Put(Bytes.toBytes(intToString(i)));
+						p1.setWriteToWAL(false);
+						
+						/*
+						 * add(byte[] family, byte[] qualifier, byte[] value) 
+						 * Add the specified column and value to this Put operation.
+						 */
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col0), Bytes.toBytes( reader.getValues()[0] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col1), Bytes.toBytes( reader.getValues()[1] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col2), Bytes.toBytes( reader.getValues()[2] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col3), Bytes.toBytes( reader.getValues()[3] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col4), Bytes.toBytes( reader.getValues()[4] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col5), Bytes.toBytes( reader.getValues()[5] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col6), Bytes.toBytes( reader.getValues()[6] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col7), Bytes.toBytes( reader.getValues()[7] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col8), Bytes.toBytes( reader.getValues()[8] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col9), Bytes.toBytes( reader.getValues()[9] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col10), Bytes.toBytes( reader.getValues()[10] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col11), Bytes.toBytes( reader.getValues()[11] ) );
+						p1.add(Bytes.toBytes(familyName), Bytes.toBytes(col12), Bytes.toBytes( reader.getValues()[12] ) );
+						
+						table.put(p1);
+						
+//						System.out.println(i);
+						
+//					}//end for
+					long end = System.currentTimeMillis();
+					total_write_time += (end - begin);
+					i++;
+//					//print every line logs
+//					System.out.println("[Thread Info]"
+//							+ " threadId = " + this.threadId
+//							+ " [Line] HBase write line : " + i);
+				}//end while
+				
+				synchronized(Exp_thoughput_57M_32Threads.tableLock){
+					table.flushCommits();
+				}
+				
+				System.out.println("[Thread Info]"
+						+ " threadId = " + this.threadId
+						+ " [Time] HBase write time : " + total_write_time);
+				System.out.println("[Thread Info]"
+						+ " threadId = " + this.threadId
+						+ " [Line] HBase write Line : " + (i-i_origin));
+				reader.close();
+			}
+			catch (Exception ex) {
+				System.out.println(ex);
+			}
+			
+			
+		}//end try
+		catch (Exception e)
+        {
+            e.printStackTrace();
+        }//end catch
+	}//end run
+	
+	
+	static public String intToString(int x) {
+		String result = String.valueOf(x);
+		int size = result.length();
+//		1000W line rowkey adapt
+		while (size < 8) {
+			size++;
+			result = "0" + result;
+		}
+		return result;
+	}
+}
